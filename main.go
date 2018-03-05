25a2b5f7ff2c8ebdfa4b5197c55cde8835b9248a
Oguangpan <panndora@deadpig.cc>
2018-03-05 16:50:50 +0800

    接近完成

--- main.go
+++ main.go
@@ -12,6 +12,10 @@ Process:
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+
 	"github.com/Oguangpan/szsspSysManage/ynm3000"
 )
 
@@ -19,8 +23,108 @@ import (
 const tmpIp = "33.66.100.255"
 
 func main() {
-	var com ynm3000.Ynm3k
-	com = new(ynm3000.Computer)
-	com.GetHardNumber()
-	com.GetMacAddress()
+	var dn *ynm3000.Computer
+	dn = new(ynm3000.Computer)
+	dn.GetHardNumber()
+	dn.GetMacAddress()
+	cowNum, _ := dn.SearchXlsx()
+	hddNum := len(dn.HddIds)
+	macNum := len(dn.MacAddress)
+	var yon string
+	var num int = 0
+	/*
+		分为两种情况, 依据mac地址为基础,第一种.如果有mac地址,而当前硬盘数据不相符,那么提示修改.
+		第二种情况是没有查询到相关mac地址,提示是否新增数据,添加一row,输入 部门 使用者, 上传 部门
+		使用者, 硬盘序列号,mac地址到服务器中, 后续ip分配工作直接编辑xlsx文件完成.
+	*/
+	var update bool
+	if cowNum == 0 {
+		// 没有查询到mac地址
+
+		stdin := bufio.NewReader(os.Stdin)
+		fmt.Println("您的电脑信息并没有在服务器记录中，是否添加新的设备信息。 yes or no ?>")
+		fmt.Scanln(&yon)
+		if yon != "yes" {
+			return
+		}
+		fmt.Println("请输入该设备使用者姓名>")
+		fmt.Scanln(&dn.User)
+		fmt.Println("请输入该设备的所属部门>")
+		fmt.Scanln(&dn.Department)
+		if hddNum > 1 {
+			fmt.Println("您的电脑拥有多个硬盘,请选择其中一个作为记录硬盘>")
+			for i, j := range dn.HddIds {
+				fmt.Println(i, ":", j)
+			}
+			fmt.Fscan(stdin, &num)
+			dn.HddId = dn.HddIds[num]
+		} else if hddNum > 1 {
+			dn.HddId = dn.HddIds[0]
+		} else {
+			fmt.Println("警告:没有发现该设备上的硬盘物理序列号")
+		}
+		if macNum > 1 {
+			fmt.Println("您的电脑中有多个网卡,请选择其中一个作为接入内网专用>")
+			for i, j := range dn.MacAddress {
+				fmt.Println(i, ": ", j["Name"], j["Mac"])
+			}
+			fmt.Fscan(stdin, &num)
+			dn.MacAddres = dn.MacAddress[num]["Mac"]
+
+		} else if macNum > 0 {
+			dn.MacAddres = dn.MacAddress[0]["Mac"]
+		} else {
+			fmt.Println("警告:没有在该设备上发现可用的网卡")
+		}
+		fmt.Println("该机器信息如下: \n用户:",
+			dn.User, "\n部门:",
+			dn.Department, "\n硬盘物理序列号:",
+			dn.HddId, "\n网卡IP地址:",
+			dn.MacAddres, "\n是否直接提交到服务器中? yes or no >")
+		fmt.Scanln(&yon)
+		if yon == "yes" {
+			dn.AmXlsx(false)
+		} else {
+			fmt.Println("您选择了no，结束提交，服务器记录中仍然没有该设备的记录。")
+		}
+		return
+
+	} else {
+		// 查询到了mac地址, 判断硬盘序列号是否与服务器
+		if hddNum > 0 {
+			update = true
+			for _, j := range dn.HddIds {
+				if j == dn.HddId {
+					update = false
+				}
+			}
+			if update {
+				fmt.Println("检查到当前电脑硬盘序列号与服务器记录不相符，是否修正？ yes or no>")
+				var q string
+				fmt.Scanln(&q)
+				if q == "yes" {
+					switch hddNum {
+					case hddNum == 1:
+						dn.HddId = dn.HddIds[0]
+					case hddNum > 1:
+						fmt.Println("您的设备中有多个硬盘，请选择需要绑定的那个。>")
+						for k, v := range dn.HddIds {
+							fmt.Println(k, ":", v)
+						}
+						fmt.Fscan(stdin, &num)
+						dn.HddId = dn.HddIds[num]
+					}
+				} else {
+					fmt.Println("您选择了no，将不对硬盘序列号记录做修改。")
+				}
+
+			} else {
+				fmt.Println("警告:该设备没有发现可用的硬盘序列号")
+			}
+
+			dn.AmXlsx(true)
+		}
+		//fmt.Println(cowNum, cardName, dn.Ip)
+
+	}
 }
--- ynm3000/ynm3000.go
+++ ynm3000/ynm3000.go
@@ -4,16 +4,16 @@ The value of this program is no value. Szssp belongs to the company
 Well, its name is called YNM3000.
 Property value should be:
    - HDD ID
-   - Device model
+   - Ip
    - mac address
-   - user
+   - User
    - Department
 The method should be:
    - Get the hard disk serial number
    - Get mac address
    - search xlsx file mac address
    - Add or modify xlsx file contents
-   - Set the ip address
+   - Set the Ip address
 */
 package ynm3000
 
@@ -22,22 +22,28 @@ import (
 	"net"
 	"os/exec"
 	"strings"
+
+	"github.com/tealeg/xlsx"
 )
+
+const xlsxFilePath = "//33.66.96.14/public/2018Taizhang.xlsx"
 
 type Ynm3k interface {
 	GetHardNumber()
 	GetMacAddress()
-	//SearchXlsx()
+	SearchXlsx() (int, string)
 	//SetIpaddress()
 	//AmXlsx()
 }
 
 type Computer struct {
-	hddId      []string
-	model      string
-	macAddress []intfInfo
-	user       string
-	department string
+	HddIds     []string            // All hard disks read on this device
+	HddId      string              // The hard disk in the form file
+	Ip         string              //The IP address stored on the form
+	MacAddress []map[string]string //This machine all the Ip address
+	MacAddres  string
+	User       string
+	Department string
 }
 
 // Get the hard disk physical serial number information
@@ -46,30 +52,79 @@ func (cp *Computer) GetHardNumber() {
 	ids := string(ids_byte)
 	var slicel []string = strings.Fields(ids)[1:]
 	for _, j := range slicel {
-		cp.hddId = append(cp.hddId, j)
+		cp.HddIds = append(cp.HddIds, j)
 	}
-	fmt.Println(cp.hddId)
+
 }
 
 // Get the Netcard information
-type intfInfo struct {
-	Name string
-	Mac  net.HardwareAddr
-}
 
 func (cp *Computer) GetMacAddress() {
 	intf, err := net.Interfaces()
 	if err != nil {
 		return
 	}
-	var tmp intfInfo
+
 	for _, v := range intf {
-		tmp.Name = v.Name
-		tmp.Mac = v.HardwareAddr
-		cp.macAddress = append(cp.macAddress, tmp)
+		tmp := make(map[string]string)
+		tmp["Name"] = v.Name
+		tmp["Mac"] = v.HardwareAddr.String()
+		if tmp["Mac"] != "" {
+			cp.MacAddress = append(cp.MacAddress, tmp)
+		}
 	}
-	// test code
-	for _, v := range cp.macAddress {
-		fmt.Println(v)
+	//fmt.Println(cp.MacAddress)
+}
+
+// Query  mac address from xlsx related data
+// Return the number of rows, device data, matching the name of the network card
+func (cp *Computer) SearchXlsx() (int, string) {
+	xlFile, err := xlsx.OpenFile(xlsxFilePath)
+	if err != nil {
+		fmt.Println(err)
+		return 0, ""
 	}
+
+	for _, sheet := range xlFile.Sheets {
+		if sheet.Name == "计算机" {
+			for column, row := range sheet.Rows {
+				for _, j := range cp.MacAddress {
+					for _, cell := range row.Cells {
+						if cell.String() == j["Mac"] {
+
+							/*The following switch to determine the basis of the column in the xlsx table information,
+							so you want to directly use the package file you also need the corresponding xlsx file,
+							or need to be changed as required.*/
+							for i, cell := range row.Cells {
+								switch i {
+								case 2:
+									cp.Department = cell.String()
+								case 3:
+									cp.User = cell.String()
+								case 9:
+									cp.HddId = cell.String()
+								case 10:
+									cp.Ip = cell.String()
+								case 11:
+									cp.MacAddres = cell.String()
+								}
+							}
+
+							return column, j["Name"]
+						}
+					}
+				}
+			}
+		}
+	}
+	return 0, ""
+}
+
+func (cp *Computer) AmXlsx(s bool) (err error) {
+
+	return nil
+}
+
+func (cp *Computer) SetIpaddress() {
+	// cp.Ip
 }
